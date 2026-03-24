a8e4288ad2d38f1f77abf27e5a28f1028eeab458
Shcherbyna <t25shcherbynana>
2026-03-24 12:32:08 +0100

    third commit

--- main.py
+++ main.py
@@ -5,23 +5,46 @@ Umsetzung der Superdepp-Aufgabe mittels Python
 
 def main() -> None:
     #Lokale Variablen
-    smsListe:[str] = ["SuperDepp =1", "superdepp= 3", "sUperdepp 5", "superdepp = 7", "superdepp = 9"
-"superdepp= 3", "Superdepp=7", "SUPERDEPP=7", "suppendepp=11", "hupersep= b", "superdepp=a", "sUperdep=c", "superdepp==7",
+    smsListe:[str] = ["SuperDepp =1", "superdepp= 3", "uperdepp=3", "sUperdepp 5", "superdepp = 7", "superdepp     =9"
+"superdepp=    3", "Superdepp=7", "SUPERDEPP=7", "suppendepp=11", "hupersep= b", "superdepp=a", "sUperdep=c", "superdepp==7",
 "superdepp-9", "superdepp 3", "superdepp..7", "pupersepp=7", "SUPERdepp= 3", "superDEPP= 5", "SUPERDEPP= 7", "SUPERDEPP= 9",
-"SUPERDEPP= 7", "SUPERDEPP= 9", "SuPeRdEpP=9"]
-    anzahlSmsListe: int = len(smsListe)
-    smsOK:[str] = []
-    anzahlSmsOK: int = 0
-    prozentSmsOK: float = 0.0
+"SUPERDEPP= 7", "SUPERDEPP= 9", "SuPeRdEpP=11"]
+    anzahlSmsListe: int = len(smsListe) #Anzahl abgegebener Stimmen
+    smsOK:[str] = [] # leere Liste für güötige SMS-Einträge
+    anzahlSmsOK: int = 0 # Anzahl gültiger Stimmen
+    prozentSmsOK: float = 0.0 # Prozentsatz gültiger Stimmen
+    kandidatenStimmenListe:[int] = [0,0,0,0,0,0,0,0,0,0,] # für 10 Kandidaten
+    platzierungsListe:[int] = [] # leere Platzierungsliste
 
-    ausgabe:str = "Auswertung Superdepp\n" + 20 * "*"
-    ausgabe += f"\nerhaltene SMS: {smsListe}\nmit {anzahlSmsListe} Eintragen"
-    ausgabe += f"\ngültige SMS: {smsOK}\nmit {anzahlSmsOK} Einträgen"
-    ausgabe += f"\ngültige SMS {0:.1%}".format(prozentSmsOK)
+    # *********** Verarbeitung nach Syntaxgraph zu SupperDepp-Stimmen *****************
+    for sms in smsListe:
+        #Prüfung auf genau ein =-Zeichen
+        if sms.count("=") ==1:
+            if "superdepp" in sms.lower(): # Prüfung, dass Superdepp enthalten ist, egal ob Groß- oder Kleinschreibung
+                teile = sms.split("=")
+                if teile[1].strip().isdigit():
+                    zahl:int = int(teile[1].strip())
+                    if zahl >= 1 and zahl <=10:
+                        smsOK.append(sms) # gültige SMS in Liste hinzufügen
+                        kandidatenStimmenListe[zahl-1] += 1 # Kandidatenstimme hinzufügen
+
+    anzahlSmsOK = len(smsOK) # ermittle Anzahl der gültigen Stimmen
+    prozentSmsOK = anzahlSmsOK*100/anzahlSmsListe
 
 
-    print(ausgabe)
+    # Ausgabetext erstellen
+    ausgabeText:str = "Auswertung Superdepp\n" + 20 * "*"
+    ausgabeText += f"\nerhaltene SMS: {smsListe}\nmit {anzahlSmsListe} Eintragen"
+    ausgabeText += f"\ngültige SMS: {smsOK}\nmit {anzahlSmsOK} Einträgen"
+    ausgabeText += f"\ngültige SMS {prozentSmsOK} %"
+    ausgabeText += f"\nKandidateiste mit Stimmenanzahl: {kandidatenStimmenListe}"
+    ausgabeText += f"\nPlatzierungsliste mit Kandidatennummern: {platzierungsListe}"
 
+
+
+
+    # Ausgabe
+    print(ausgabeText)
 
 
 # Einstiegspunkt Hauptprogramm
