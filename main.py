690bf82bfb132217323771b5fe6ca70e71719b39
Shcherbyna <t25shcherbynana>
2026-03-24 11:40:23 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+Umsetzung der Superdepp-Aufgabe mittels Python
+24.03.26 IGy25
+"""
+
+def main() -> None:
+    #Lokale Variablen
+    smsListe:[str] = ["SuperDepp =1", "superdepp= 3", "sUperdepp 5", "superdepp = 7", "superdepp = 9"
+"superdepp= 3", "Superdepp=7", "SUPERDEPP=7", "suppendepp=11", "hupersep= b", "superdepp=a", "sUperdep=c", "superdepp==7",
+"superdepp-9", "superdepp 3", "superdepp..7", "pupersepp=7", "SUPERdepp= 3", "superDEPP= 5", "SUPERDEPP= 7", "SUPERDEPP= 9",
+"SUPERDEPP= 7", "SUPERDEPP= 9", "SuPeRdEpP=9"]
+    anzahlSmsListe: int = len(smsListe)
+    smsOK:[str] = []
+    anzahlSmsOK: int = 0
+    prozentSmsOK: float = 0.0
+
+    ausgabe:str = "Auswertung Superdepp\n" + 20 * "*"
+    ausgabe += f"\nerhaltene SMS: {smsListe}\nmit {anzahlSmsListe} Eintragen"
+
+
+    print(ausgabe)
+
+
+
+# Einstiegspunkt Hauptprogramm
+if __name__ == '__main__':
+    main()
